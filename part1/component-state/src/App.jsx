e3819fdb14c50477de3a9bb1f16344034199266b
JhoniSanchez-Intellisys <jsanchezs@intellisys.com.do>
2024-01-23 10:19:56 -0400

    estado del componente

---
+++ part1/component-state/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+// const Hello = (props) => {
+
+//   const bornYear = () => {
+//     const yearNow = new Date().getFullYear()
+//     return yearNow - props.age
+//   }
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {props.name}, you are <b>{props.age} years old</b>
+//       </p>
+//       <p>So you were probably born in <i>{bornYear()}</i></p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   )
+// }
+
+
+// export default App
--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Header(props) {
+  return <div>{props.course}</div>;
 }
 
-export default App
+function Part(props) {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+}
+
+function Content(props) {
+  return (
+    <div>
+      <Part part={props.p1} exercises={props.e1} />
+      <Part part={props.p2} exercises={props.e2} />
+      <Part part={props.p3} exercises={props.e3} />
+    </div>
+  );
+}
+
+function Footer(props) {
+  return <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>;
+}
+
+///////////////////////
+
+
+
+
+
+const App = () => {
+  const course = "Half Stack application development";
+  // const part1 = "Fundamentals of React";
+  // const exercises1 = 10;
+  // const part2 = "Using props to pass data";
+  // const exercises2 = 7;
+  // const part3 = "State of a component";
+  // const exercises3 = 14;
+
+  // const part1 = {
+  //   name: 'Fundamentals of React',
+  //   exercises: 10
+  // }
+  // const part2 = {
+  //   name: 'Using props to pass data',
+  //   exercises: 7
+  // }
+  // const part3 = {
+  //   name: 'State of a component',
+  //   exercises: 14
+  // }
+
+  const part1 = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        p1={part1[0].name}
+        e1={part1[0].exercises}
+        p2={part1[1].name}
+        e2={part1[1].exercises}
+        p3={part1[2].name}
+        e3={part1[2].exercises}
+      />
+      <Footer e1={part1[0].exercises} e2={part1[1].exercises} e3={part1[2].exercises} />
+    </div>
+  );
+};
+
+export default App;
