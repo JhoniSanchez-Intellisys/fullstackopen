e81af1ad234d3d47a62940da7b555bc6ec5899b7
JhoniSanchez-Intellisys <jsanchezs@intellisys.com.do>
2024-01-25 09:25:07 -0400

    unicafe corregido porciento

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 function Header(props) {
-  return <div>{props.course}</div>;
+  return <h1>{props.course}</h1>;
 }
 
 function Part(props) {
@@ -20,65 +20,45 @@ function Content(props) {
   );
 }
 
-function Footer(props) {
+function Total(props) {
   return <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>;
 }
 
-///////////////////////
-
-
-
-
-
 const App = () => {
-  const course = "Half Stack application development";
-  // const part1 = "Fundamentals of React";
-  // const exercises1 = 10;
-  // const part2 = "Using props to pass data";
-  // const exercises2 = 7;
-  // const part3 = "State of a component";
-  // const exercises3 = 14;
-
-  // const part1 = {
-  //   name: 'Fundamentals of React',
-  //   exercises: 10
-  // }
-  // const part2 = {
-  //   name: 'Using props to pass data',
-  //   exercises: 7
-  // }
-  // const part3 = {
-  //   name: 'State of a component',
-  //   exercises: 14
-  // }
-
-  const part1 = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    part1: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
       <Content
-        p1={part1[0].name}
-        e1={part1[0].exercises}
-        p2={part1[1].name}
-        e2={part1[1].exercises}
-        p3={part1[2].name}
-        e3={part1[2].exercises}
+        p1={course.part1[0].name}
+        e1={course.part1[0].exercises}
+        p2={course.part1[1].name}
+        e2={course.part1[1].exercises}
+        p3={course.part1[2].name}
+        e3={course.part1[2].exercises}
       />
-      <Footer e1={part1[0].exercises} e2={part1[1].exercises} e3={part1[2].exercises} />
+      <Total
+        e1={course.part1[0].exercises}
+        e2={course.part1[1].exercises}
+        e3={course.part1[2].exercises}
+      />
     </div>
   );
 };
