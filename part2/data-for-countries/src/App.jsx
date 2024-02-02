d196a4b9cf238c23cbb2d91f365d5fe2c5e5cca9
JhoniSanchez-Intellisys <jsanchezs@intellisys.com.do>
2024-02-02 16:05:48 -0400

    Avance

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [countries, setcountries] = useState([]);
-  const [search, setSearch] = useState([]);
+  const [countriesf, setcountriesf] = useState([]);
+  const [search, setSearch] = useState("");
 
   const nation = async () => {
     const data = await fetch(
@@ -12,22 +13,78 @@ function App() {
     );
     const res = await data.json();
     console.log(res);
-    setcountries(res)
+    setcountries(res);
+    setcountriesf(res);
   };
 
   useEffect(() => {
     nation();
+
     //     .then((res) => {
     // return res.json().then((el)=>setcountries(el.data))
   }, []);
 
+  useEffect(() => {
+    data();
+    // nation();
+    //     .then((res) => {
+    // return res.json().then((el)=>setcountries(el.data))
+  }, [search]);
+
+  const find = (e) => {
+    const valueT = e.target.value;
+    console.log(valueT);
+    console.log(typeof valueT);
+    setSearch(valueT);
+    console.log(search);
+  };
+
+  const data = () => {
+    const fil = countries.filter((el) => {
+      return el.name.common.toLowerCase().includes(search.toLowerCase());
+    });
+    if (search.length == 0) {
+      setcountriesf(countries);
+    } else if (fil.length >= 10) {
+      setcountriesf([
+        { name: { common: "Too many matches, specify another filter" } },
+      ]);
+      console.log(search);
+    } else if (fil.length < 10) {
+      setcountriesf(fil);
+    } else {
+      setcountriesf([]);
+    }
+    console.log(fil);
+  };
+
   return (
     <>
-      <div>Find Countries</div> <input type="text" />
+      <div>Find Countries</div>
+      <input
+        value={search}
+        onChange={(e) => {
+          find(e);
+        }}
+        type="text"
+      />
       <div>
-        {countries.map((el) => {
-          return <li>{el.name.common}</li>;
-        })}
+        {countriesf.length == 1 ? (
+          <li key={1}>
+            <h2>{countriesf[0].name.common}</h2>
+            Capital: {countriesf[0].capital[0]}
+            <p>Area:{countriesf[0].area}</p>
+            <img src={countriesf[0].flags.png}></img>
+            <h4>Languages</h4>
+            {Object.values(countriesf[0].languages).map((el, i) => {
+              return <li key={i}>{el}</li>;
+            })}
+          </li>
+        ) : (
+          countriesf.map((el, i) => {
+            return <div key={i}>{el.name.common}gti</div>;
+          })
+        )}
       </div>
     </>
   );
