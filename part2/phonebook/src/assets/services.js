1c3413b8af1d4d7c1a87660577bfaade2b0d2fa7
JhoniSanchez-Intellisys <jsanchezs@intellisys.com.do>
2024-02-08 09:37:33 -0400

    Render

--- part2/phonebook/src/assets/services.js
+++ part2/phonebook/src/assets/services.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+// const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "https://one234-sola.onrender.com/api/persons";
+
 
 const getAll = () => {
   const request = axios.get(baseUrl);
