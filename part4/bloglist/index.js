337b72f8e1af679db76102f449b2d94c5231f5eb
JhoniSanchez-Intellisys <jsanchezs@intellisys.com.do>
2024-02-15 16:34:02 -0400

    Avance parte 4 apartado b

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,11 +3,15 @@ const app = express();
 const cors = require("cors");
 const Blog = require("./models/modelblog.js");
 const blogreutes = require("./controllers/blogcontrollers.js")
+const middleware = require("./utils/middleware.js")
 require("./app.js");
 
 app.use(cors());
 app.use(express.json());
 app.use("/", blogreutes)
+app.use(middleware.errorHandler);
+app.use(middleware.requestLogger)
+app.use(middleware.unknownEndpoint);
 
 const blog = new Blog({
   title: "Casa",
